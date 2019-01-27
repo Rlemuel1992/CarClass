398d34c4fe27d5eb956f4fd470bed0103d55e0e0
Ronald Lemuel <Rlemuel1992@gmail.com>
2019-01-26 22:33:30 -0500

    first commit

---
+++ CarClass/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+	class Car
+	{
+		public string make = "un-defined";
+		public string model = "un-defined";
+		public int year = 1786;
+		public double price = 19.99;
+
+		public Car(string make, string model, int year, double price)
+		{
+			this.make = make;
+			this.model = model;
+			this.year = year;
+			this.price = price;
+		}
+
+		public void PrintInfo()
+		{
+			Console.WriteLine("\n");
+			Console.WriteLine("Vehicle information\n" +
+							  "___________________\n" +
+							  "Make: " + make  +
+							  "\nModel: " + model +
+							  "\nYear: " + year +
+							  "\nMSRP: " + price );
+		}
+
+	}
+}
---
+++ CarClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("How many vechicles would you like to input?");
+
+			int.TryParse(Console.ReadLine(), out int numOfCars);
+			List<Car> carsList = new List<Car>(numOfCars);
+		
+				for (int i = 0; i < numOfCars; i++)
+				{
+					string make = "un-defined";
+					string model = "un-defined";
+					int year = 1786;
+					double price = 101.4008;
+
+					Console.WriteLine("Please input some information for the vehicle");
+					Console.WriteLine("Make");
+					make = Console.ReadLine();
+					Console.WriteLine("Model");
+					model = Console.ReadLine();
+					Console.WriteLine("Year");
+					int.TryParse(Console.ReadLine(), out year);
+					Console.WriteLine("Price");
+					double.TryParse(Console.ReadLine(), out price);
+					carsList.Add(new Car(make, model, year, price));
+				}
+
+			foreach (Car c in carsList)
+			{
+				c.PrintInfo();
+			}
+			
+		}
+	}
+}
